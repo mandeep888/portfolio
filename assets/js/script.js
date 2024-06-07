c99fb20bdf973bb5fbc91e49129b0ef9a62c5fac
Mandeep Singh Jass <102341968+mandeep888@users.noreply.github.com>
2024-06-08 00:27:09 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Frontend development ", "Backend development ", "Web designing ", "Full Stack Development ", "Web development "],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
