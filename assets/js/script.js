99688505413fdd5a2ad078ee150b6222b1f7cc50
Mandeep Singh Jass <102341968+mandeep888@users.noreply.github.com>
2024-06-08 10:37:50 +0530

    removed title bug in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Mandeep Singh Jass";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "assets/images/favicon.ico");
         }
         else {
             document.title = "Come Back To Portfolio";
